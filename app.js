90c8ee6ac762d7b3208307f3a10c3ef1ee62575c
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2022-08-18 18:00:50 +0100

    targetting the html elements

---
+++ app.js
@@ -0,0 +1,5 @@
+const p1Display = document.querySelector('#p1display');
+const p2Display = document.querySelector('#p2display');
+const p1Btn = document.querySelector('#p1button');
+const p2Btn = document.querySelector('#p2button');
+
