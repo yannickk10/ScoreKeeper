21b347b0c238e2a100e4ad1ac15389cdf0064b11
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2022-08-21 13:25:32 +0100

    adding css prpoerties to our elements

--- app.js
+++ app.js
@@ -5,18 +5,24 @@ const p2Btn = document.querySelector('#p2button');
 const resetBtn = document.querySelector('#reset');
 const winningScore = document.querySelector('#playto');
 
-p1Score = 0;
-p2Score = 0;
-upToScore = 0;
-isGameOver = false;
+let p1Score = 0;
+let p2Score = 0;
+let upToScore = 0;
+let isGameOver = false;
 
 p1Btn.addEventListener('click', () => {
-    // if the isGameOver is true increase the score by 1
+    // if the isGameOver is not true increase the score by 1
     if (!isGameOver) {
         p1Score += 1;
-        // if the score is equal to the selected winning score turn 
+        // if the score is equal to the selected winning score turn isGameover to true
         if (p1Score === upToScore) {
             isGameOver = true;
+            //changing the score colours when game over
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            //disabling buttons
+            p1Btn.disabled = true;
+            p2Btn.disabled = true;
         }
     }
     //edit the score display
@@ -29,6 +35,11 @@ p2Btn.addEventListener('click', () => {
         // if the score is equal to the selected winning score turn 
         if (p2Score === upToScore) {
             isGameOver = true;
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+
+            p1Btn.disabled = true;
+            p2Btn.disabled = true;
         }
     }
     // update the score display
@@ -43,12 +54,17 @@ winningScore.addEventListener('change', function () {
 
 // resetting the values and game
 resetBtn.addEventListener('click', () => {
+    isGameOver = false;
     p1Score = 0;
     p2Score = 0;
 
     // updating the score display
-    p1Display.textContent = `${p1Score}`;
-    p2Display.textContent = `${p2Score}`;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
 
-    isGameOver = false;
+    p1Display.classList.remove('has-text-success');
+    p2Display.classList.remove('has-text-danger');
+
+    p1Btn.disabled = false;
+    p2Btn.disabled = false;
 })
