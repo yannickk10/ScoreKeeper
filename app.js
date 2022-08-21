626f7f957809ad892c58270951261e82c26e9362
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2022-08-21 14:07:29 +0100

    refactoring the code

--- app.js
+++ app.js
@@ -1,49 +1,46 @@
-const p1Display = document.querySelector('#p1display');
-const p2Display = document.querySelector('#p2display');
-const p1Btn = document.querySelector('#p1button');
-const p2Btn = document.querySelector('#p2button');
+//player object
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1button'),
+    display: document.querySelector('#p1display')
+}
+
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2button'),
+    display: document.querySelector('#p2display')
+}
+
 const resetBtn = document.querySelector('#reset');
 const winningScore = document.querySelector('#playto');
-
-let p1Score = 0;
-let p2Score = 0;
 let upToScore = 0;
 let isGameOver = false;
 
-p1Btn.addEventListener('click', () => {
+function updateScores(player, opponent) {
     // if the isGameOver is not true increase the score by 1
     if (!isGameOver) {
-        p1Score += 1;
+        player.score += 1;
         // if the score is equal to the selected winning score turn isGameover to true
-        if (p1Score === upToScore) {
+        if (player.score === upToScore) {
             isGameOver = true;
             //changing the score colours when game over
-            p1Display.classList.add('has-text-success');
-            p2Display.classList.add('has-text-danger');
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
             //disabling buttons
-            p1Btn.disabled = true;
-            p2Btn.disabled = true;
+            player.button.disabled = true;
+            opponent.button.disabled = true;
         }
     }
     //edit the score display
-    p1Display.textContent = `${p1Score}`; 
-})
-p2Btn.addEventListener('click', () => {
-    // if the isGameOver is true increase the score by 1
-    if (!isGameOver) {
-        p2Score += 1;
-        // if the score is equal to the selected winning score turn 
-        if (p2Score === upToScore) {
-            isGameOver = true;
-            p1Display.classList.add('has-text-success');
-            p2Display.classList.add('has-text-danger');
+    player.display.textContent = `${player.score}`; 
+}
 
-            p1Btn.disabled = true;
-            p2Btn.disabled = true;
-        }
-    }
-    // update the score display
-    p2Display.textContent = `${p2Score}`; 
+
+p1.button.addEventListener('click', () => {
+    updateScores(p1,p2);
+})
+p2.button.addEventListener('click', () => {
+    updateScores(p2,p1);
 })
 
 // checking if there's a change of event (the user selects a different number)
@@ -55,16 +52,12 @@ winningScore.addEventListener('change', function () {
 // resetting the values and game
 resetBtn.addEventListener('click', () => {
     isGameOver = false;
-    p1Score = 0;
-    p2Score = 0;
-
-    // updating the score display
-    p1Display.textContent = 0;
-    p2Display.textContent = 0;
-
-    p1Display.classList.remove('has-text-success');
-    p2Display.classList.remove('has-text-danger');
-
-    p1Btn.disabled = false;
-    p2Btn.disabled = false;
+    //for every player object
+    for(let p of [p1, p2]) {
+        p.score = 0;
+        // updating the score display, removing text colour & enabling buttons 
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+    }
 })
